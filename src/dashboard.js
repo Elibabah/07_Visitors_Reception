feddd418218131453575c7ee75e712fec19d2d26
Elibabah <elibabah@localhost.localdomain>
2021-12-21 18:28:14 -0600

    implementando modal

--- src/dashboard.js
+++ src/dashboard.js
@@ -11,7 +11,37 @@ window.addEventListener("DOMContentLoaded", async(e) => {
         console.log(doc.data().nombre);
         dataAdmin.push(doc.data());
         console.log(dataAdmin);
+
+        let lista = doc.data();
+
+        document.getElementById("visitantesEnLista").innerHTML += `
+            <tr id="listaPersonas">
+                <td id="persona" value="${lista}" onclick="${lista} data-bs-toggle="modal" data-bs-target="#staticBackdrop">${lista.nombre}</td>
+                <td>${lista.empresa}</td>
+            </tr>
+    <!-- Modal -->
+    <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    ...
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Understood</button>
+                </div>
+            </div>
+        </div>
+    </div>
+`;
     });
 });
-
 console.log(dataAdmin);
+/*----------- Iterar list visitantes ----------*/
+/*for (let i = 0; i < dataAdmin.length; i++) {
+    console.log(i)
+}*/
